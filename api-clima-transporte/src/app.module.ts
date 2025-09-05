bb1c13dda9e69db2119ceafff698b52a193086d6
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-09-05 12:46:58 -0300

    feat(infra): Docker Compose (Postgres/Redis) e Prisma (schema inicial, módulo/serviço, migration)

--- api-clima-transporte/src/app.module.ts
+++ api-clima-transporte/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health.controller';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infra/database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
