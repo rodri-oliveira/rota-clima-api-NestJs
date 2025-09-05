c57a940166ec1429ceeae4de01673c38ddb67a23
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-09-05 12:34:31 -0300

    feat(core): adiciona endpoint /health e documentação Swagger em /docs

--- api-clima-transporte/src/main.ts
+++ api-clima-transporte/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuração Swagger (documentação da API)
+  const config = new DocumentBuilder()
+    .setTitle('API Clima + Transporte')
+    .setDescription(
+      'API que consolida rotas de transporte e previsão do clima, com histórico e favoritos.',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
